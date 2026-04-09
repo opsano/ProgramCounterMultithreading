4e610ade02fe839b53befa5149ce73ee3b476341
opsano <greennates@gmail.com>
2026-04-09 14:39:21 -0700

    Finished timer

--- src/DisplayThread.java
+++ src/DisplayThread.java
@@ -1,17 +1,35 @@
 import java.util.concurrent.atomic.*;
 public class DisplayThread implements Runnable{
     AtomicInteger programsRanPerSecond = new AtomicInteger();
-    AtomicInteger totalProgramsToRun = new AtomicInteger();
+    AtomicInteger timeRan = new AtomicInteger();
     AtomicInteger totalProgramsRan = new AtomicInteger();
     public void run(){
         try {
             while(true){
                 Thread.sleep(1000);
-                System.out.printf("|%d - Operations Per Second|\n|%d - Total Operations Performed|\n",programsRanPerSecond.get(),totalProgramsRan.get());
+                timeRan.incrementAndGet();
+                System.out.printf("------------------------------\n" +
+                                "|%d - Operations Per Second\n|%d - Total Operations Performed\n|%s" +
+                                "\n------------------------------\n",
+                        programsRanPerSecond.get(),totalProgramsRan.get(),getFormattedTime());
                 programsRanPerSecond.set(0);
             }
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+    }
+    private String getFormattedTime(){
+        int hours, minutes, seconds;
+        hours = minutes = 0;
+        seconds = timeRan.get();
+        while (seconds >= 60){
+            seconds -= 60;
+            minutes++;
+        }
+        while (minutes >= 60){
+            minutes -= 60;
+            hours++;
+        }
+        return String.format("%02d:%02d:%02d - Total Time Elapsed",hours, minutes, seconds);
     }
 }
